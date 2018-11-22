f96f768c81ea85b1fe6e9830c6ee4484ddeb142a
Adam Nagy <adam.nagy@autodesk.com>
2018-11-22 11:27:40 +0000

    DotNetCoreConsole app

---
+++ DotNetCoreConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace NodeModulesCleaner
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var folder = args[0];
+      if (!Directory.Exists(folder))
+      {
+        Console.WriteLine("Folder '" + folder + "' does not exist!");
+        return;
+      }
+
+      Console.WriteLine("Started scanning folder '" + folder + "'");
+
+      CheckFolder(folder);
+
+      Console.WriteLine("Finished!");
+    }
+
+    static private void CheckFolder(string folderPath)
+    {
+      var files = Directory.EnumerateFiles(folderPath);
+      foreach (var file in files)
+      {
+        // If it's a js file but not a minified version
+        if (file.EndsWith(".js") && !file.EndsWith(".min.js"))
+        {
+          // If there is a minified version of the file then delete the 
+          // non-minified version
+          if (File.Exists(file.Replace(".js", ".min.js")))
+          {
+            File.Delete(file);
+            Console.WriteLine("Deleted: '" + file + "'");
+          }
+        }
+      }
+
+      // Check subfolders
+      var folders = Directory.EnumerateDirectories(folderPath);
+      foreach (var folder in folders)
+      {
+        CheckFolder(folder);
+      }
+    }
+  }
+}
